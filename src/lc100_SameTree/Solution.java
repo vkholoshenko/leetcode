9e497d23b06e1e7b3032b5c7b00011990c240a7e
Volodymyr Kholoshenko <v.kholoshenko@gmail.com>
2020-05-25 18:30:41 +0300

    100. Same Tree

---
+++ src/lc100_SameTree/Solution.java
@@ -0,0 +1,12 @@
+package lc100_SameTree;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return
+                (p == null && q == null) ||
+                        (p != null && q != null &&
+                                p.val == q.val &&
+                                isSameTree(p.left, q.left) &&
+                                isSameTree(p.right, q.right));
+    }
+}
