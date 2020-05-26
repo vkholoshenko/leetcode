01b4e91a316bb34b4609f7f72de95a2fdfb69855
Volodymyr Kholoshenko <v.kholoshenko@gmail.com>
2020-05-26 18:13:37 +0300

    190. Reverse Bits

---
+++ src/TestTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+    @Test
+    void test() {
+        System.out.println(0b00000010100101000001111010011100);
+    }
+}
