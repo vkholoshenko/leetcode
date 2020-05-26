1417e6422fcf0261c35d0183e6728c58638d4518
Volodymyr Kholoshenko <v.kholoshenko@gmail.com>
2020-05-26 18:42:34 +0300

    206. Reverse Linked List

---
+++ src/lc206_ReverseLinkedList/ListNode.java
@@ -0,0 +1,9 @@
+package lc206_ReverseLinkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
