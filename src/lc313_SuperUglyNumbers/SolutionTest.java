82cb6f97730317621c6b3b955bfc1f3ef030cc8b
Volodymyr Kholoshenko <v.kholoshenko@gmail.com>
2020-05-29 14:16:46 +0300

    speedup tests :)

--- src/lc313_SuperUglyNumbers/SolutionTest.java
+++ src/lc313_SuperUglyNumbers/SolutionTest.java
@@ -11,7 +11,7 @@ class SolutionTest {
         Solution s = new Solution();
 
         int array1[] = {7,19,29,37,41,47,53,59,61,79,83,89,101,103,109,127,131,137,139,157,167,179,181,199,211,229,233,239,241,251};
-        for (int i=0; i < 100; i++) {
+        for (int i=0; i < 1; i++) {
             assertEquals(1092889481, s.nthSuperUglyNumber(100000, array1));
         }
 
