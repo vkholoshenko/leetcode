de21c97f004c9417358df6da1792d67bfe97e763
Volodymyr Kholoshenko <v.kholoshenko@gmail.com>
2020-06-25 21:31:36 +0300

    315 Count of Smaller Numbers After Self

---
+++ src/lc315CountSmallerNumbersAfterSelf/SolutionTest.java
@@ -0,0 +1,13 @@
+package lc315CountSmallerNumbersAfterSelf;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void countSmaller() {
+        System.out.println(new Solution().countSmaller(new int[]{5,2,6,1}));
+    }
+}
